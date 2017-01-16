1496941aeebc25fba46209b5ebba37b06cb3eb1b
Rory Mulligan <rorticus@gmail.com>
2017-01-16 09:24:05 -0500

    Not using sleep for tests, meta-70

--- tests/functional/asyncAwait.ts
+++ tests/functional/asyncAwait.ts
@@ -1,14 +1,15 @@
 import * as assert from 'intern/chai!assert';
 import * as registerSuite from 'intern!object';
 
+import pollUntil = require('intern/dojo/node!leadfoot/helpers/pollUntil');
+
 registerSuite({
 	'Async/Await with Bluebird'(this: any) {
 		return this.remote
 			.get((<any> require).toUrl('./bluebird.html'))
-			.sleep(1000)
-			.execute(() => {
+			.then(pollUntil<any>(function () {
 				return (<any> window).callbackValue;
-			})
+			}, undefined, 5000), undefined)
 			.then((callbackValue: number) => {
 				assert.equal(callbackValue, 42);
 			});
@@ -17,10 +18,9 @@ registerSuite({
 	'Async/Await with Dojo'(this: any) {
 		return this.remote
 			.get((<any> require).toUrl('./asyncAwait.html'))
-			.sleep(1000)
-			.execute(() => {
+			.then(pollUntil<any>(function () {
 				return (<any> window).callbackValue;
-			})
+			}, undefined, 5000), undefined)
 			.then((callbackValue: number) => {
 				assert.equal(callbackValue, 42);
 			});
@@ -29,10 +29,9 @@ registerSuite({
 	'Async/Await with Bluebird and Dojo'(this: any) {
 		return this.remote
 			.get((<any> require).toUrl('./bluebirdAndDojo.html'))
-			.sleep(1000)
-			.execute(() => {
+			.then(pollUntil<any>(function () {
 				return (<any> window).callbackValue;
-			})
+			}, undefined, 5000), undefined)
 			.then((callbackValue: number) => {
 				assert.equal(callbackValue, 42);
 			});
