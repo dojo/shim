717c758f8d838bf480f9645600e3526b6af4c89a
Rory Mulligan <rmulligan@sitepen.com>
2017-10-11 14:25:20 -0400

    Adding downlevel iteration

--- src/tslib.ts
+++ src/tslib.ts
@@ -1,2 +1,45 @@
+import global from './global';
+
+/**
+ * Provide any overrides and then load the TypeScript helpers.
+ */
+global.__values = function (o: any) {
+	let m = typeof Symbol === 'function' && o[ Symbol.iterator ], i = 0;
+	if (m) {
+		return m.call(o);
+	}
+
+	if (typeof o === 'string') {
+		const l = o.length;
+
+		return {
+			next: function () {
+				if (i >= l) {
+					return { done: true };
+				}
+
+				let char = o[ i++ ];
+				if (i < l) {
+					let code = char.charCodeAt(0);
+					if ((code >= 0xD800) && (code <= 0xDBFF)) {
+						char += o[ i++ ];
+					}
+				}
+
+				return { value: char, done: false };
+			}
+		};
+	}
+
+	return {
+		next: function () {
+			if (o && i >= o.length) {
+				o = void 0;
+			}
+			return { value: o && o[ i++ ], done: !o };
+		}
+	};
+};
+
 // load typescript helpers
 import 'tslib';
