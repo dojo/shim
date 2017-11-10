4cbf61010a02a22ce4bc79d1b51c455878b445f9
Rory Mulligan <rmulligan@sitepen.com>
2017-11-10 14:59:34 -0500

    Adding for..of tests back

--- tests/unit/iterator.ts
+++ tests/unit/iterator.ts
@@ -5,6 +5,85 @@ const { registerSuite } = intern.getInterface('object');
 const { assert } = intern.getPlugin('chai');
 
 registerSuite('iterator', {
+	'for..of': {
+		'strings'() {
+			const str = 'abcdefg';
+			const results: string[] = [];
+			const expected = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ];
+
+			for (const value of str) {
+				results.push(value);
+			}
+
+			assert.deepEqual(results, expected);
+		},
+
+		'double byte strings'() {
+			const str = '¯\\_(ツ)_/¯';
+			const results: string[] = [];
+			const expected = [ '¯', '\\', '_', '(', 'ツ', ')', '_', '/', '¯' ];
+
+			for (const value of str) {
+				results.push(value);
+			}
+
+			assert.deepEqual(results, expected);
+		},
+
+		'utf-16'() {
+			const str = '\uD801\uDC00';
+			const results: string[] = [];
+			const expected = [ '\uD801\uDC00' ];
+
+			for (const value of str) {
+				results.push(value);
+			}
+
+			assert.deepEqual(results, expected);
+		},
+
+		'arrays'() {
+			const array: any[] = [ 'foo', 'bar', {}, 1, [ 'qat', 2 ] ];
+			const results: any[] = [];
+
+			for (const value of array) {
+				results.push(value);
+			}
+
+			assert.deepEqual(results, array);
+			assert.notStrictEqual(results, array);
+		},
+
+		'iterable'() {
+			let counter = 0;
+			const iterable = {
+				[Symbol.iterator]() {
+					return {
+						next(): {value: number | undefined, done: boolean}  {
+							counter++;
+							if (counter < 5) {
+								return {
+									value: counter,
+									done: false
+								};
+							}
+							else {
+								return { done: true, value: undefined };
+							}
+						}
+					};
+				}
+			};
+			const results: (undefined | number)[] = [];
+			const expected = [ 1, 2, 3, 4 ];
+
+			for (const value of iterable) {
+				results.push(value);
+			}
+
+			assert.deepEqual(results, expected);
+		}
+	},
 	'forOf': {
 
 		'strings'() {
